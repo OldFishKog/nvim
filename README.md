cd6f669f827f3de3e2fb1669737c2f9d3195f690
oldfish <121704835+OldFishKog@users.noreply.github.com>
2026-02-19 17:20:45 +0800

    fix border

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ SomeConfig
 
 需要先安装 [nvim plug](https://github.com/junegunn/vim-plug)
 
-直接在.config拉取该仓库
+直接在.config拉取该仓库.
--- lua/config/cmpConfig.lua
+++ lua/config/cmpConfig.lua
@@ -16,8 +16,8 @@
       end,
     },
     window = {
-       completion = cmp.config.window.bordered(),
-       documentation = cmp.config.window.bordered(),
+       completion = cmp.config.window.bordered({border='rounded'}),
+       documentation = cmp.config.window.bordered({border='rounded'}),
     },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
